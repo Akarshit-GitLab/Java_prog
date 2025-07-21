d7a62f3bc35b5286c633f260eebbeab65065b62d
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-21 22:25:58 +0530

    sc

--- src/Challange/practice_question.java
+++ src/Challange/practice_question.java
@@ -1,5 +1,6 @@
 package Challange;
 
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class practice_question {
@@ -11,5 +12,6 @@ public class practice_question {
         int num2 = input.nextInt();
         int sum = num1 + num2;
         System.out.println("sum of two number " +sum);
+        System.out.println("hey");
     }
 }

77634d8eafaa845bd6bf9f1d0caabf670f3e4542
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-22 21:31:04 +0530

    sc

--- src/Challange/practice_question.java
+++ src/Challange/practice_question.java
@@ -11,6 +11,7 @@ public class practice_question {
         System.out.print("Enter  2nd number : ");
         int num2 = input.nextInt();
         int sum = num1 + num2;
+
         System.out.println("sum of two number " +sum);
         System.out.println("hey");
     }

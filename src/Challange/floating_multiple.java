240f086a299ebbb6d9e5880aebc35f68dfe60c2a
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-24 18:29:58 +0530

    sc

---
+++ src/Challange/floating_multiple.java
@@ -0,0 +1,14 @@
+package Challange;
+
+import java.util.Scanner;
+
+public class floating_multiple {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.print("enter a = ");
+        float a = input.nextFloat();
+        System.out.print("enter b = ");
+        float b = input.nextFloat();
+        System.out.println("Multiplication of a and b = " +(a*b));
+    }
+}

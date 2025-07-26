5fbe972a03830802a4002c8dc727ac8091738744
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-26 13:10:46 +0530

    sc

--- src/comments.java
+++ src/comments.java
@@ -6,7 +6,7 @@ public class comments {
         mutlti line comment
          */
         /**
-         * this java documents i am
+         * this java documents i am Akarshit
          */
     }
 }

4407e3490e13671c346b518a85e10a8bd7701712
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-08-12 22:27:29 +0530

    sc

--- src/Project/Tic_Tac_Toe_Game.java
+++ src/Project/Tic_Tac_Toe_Game.java
@@ -111,7 +111,7 @@ public class Tic_Tac_Toe_Game {
                     printBoard();
                     winner = checkWinner();
                 } else {
-                    System.out.println("Slot already taken; re-enter slot :");
+                    System.out.println("Slot already taken; re-enter slot number:");
                 }
 
             } catch (InputMismatchException e) {

0b01d7e3f322e749b8c555980d12403cafd94f6e
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-08-31 21:53:49 +0530

    sc

---
+++ src/Challange/Array_problems_1.java
@@ -0,0 +1,32 @@
+package Challange;
+
+import java.util.Scanner;
+
+//        Create a program to find the sum and avg of all elements in an array
+public class Array_problems_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("welcome to sum and average ");
+        int[] numArray = ArrayUtility.inputArray();
+        long sum =sum(numArray);
+        double avg = average(numArray);
+        System.out.println("Sum of array = "+ sum);
+        System.out.println("average of array = " + avg);
+
+    }
+    public static long sum (int[] numArray){
+        long sum = 0;
+        int i =0;
+        while (i < numArray.length ){
+            sum += numArray[i];
+            i++;
+        }
+
+        return sum;
+    }
+    public static int average (int[] numArray){
+        long sum =sum(numArray);
+
+        return (int) (sum/numArray.length);
+    }
+}
